d96de327541e18601b973d382dfd65a8f6b01d8c
Ali-Hadi-Saker <alihady530@gmail.com>
2023-10-29 07:46:10 +0200

    creating display menu

--- assignment3.py
+++ assignment3.py
@@ -4,7 +4,11 @@ Created on Sat Oct 28 23:40:05 2023
 
 @author: 1
 """
-
+def display_menu():
+    print("1.Add Matrices \n" + "2.Check Rotation \n" + "3.Invert Dictionary \n" + "4.Convert Matrix to Dictionary \n" + "5.Check Palindrome \n" + "6.Search for an Element and Merge sort \n" + "7.Exit")
 def main():
     user_name = input("please enter your name: ")
-    print("Welcome ", user_name)
+    print("Welcome", user_name)
+    display_menu()
+main()
+        
