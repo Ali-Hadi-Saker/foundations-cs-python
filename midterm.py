4ee0717d3d8e896397a857f261a2fa030a71c2a9
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-09 22:23:07 +0200

    starting the main function

--- midterm.py
+++ midterm.py
@@ -7,5 +7,18 @@ Created on Thu Nov  9 21:58:10 2023
 
 
 def displayMenu():
-    print("1. Opeb Tab \n" + "2. Close Tab \n" + "3. Switch Tab \n" + "4. Display All Tabs \n" +
-          "5. Open Nested Tab \n" + "6. Clear All Tabs \n" + "7. Save Tabs \n" + "8. Import Tabs \n" + "9. Exit")
+    print("1. Opeb Tab \n" + "2. Close Tab \n" + "3. Switch Tab \n" +
+          "4. Display All Tabs \n" + "5. Open Nested Tab \n" +
+          "6. Clear All Tabs \n" + "7. Save Tabs \n" + "8. Import Tabs \n" +
+          "9. Exit")
+
+
+def main():
+    print("Welcome to Advanced Browser Tabs Simulation")
+    choice = 0
+    while choice != 9:
+        displayMenu()
+        choice = int(input("Please enter your choice: "))
+
+
+main()
