c4c9776f1420cbf1248b275197b53fc39d009a6d
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-09 22:13:48 +0200

    Creating display menu

--- midterm.py
+++ midterm.py
@@ -5,4 +5,7 @@ Created on Thu Nov  9 21:58:10 2023
 @author: 1
 """
 
-print("hii")
+
+def displayMenu():
+    print("1. Opeb Tab \n" + "2. Close Tab \n" + "3. Switch Tab \n" + "4. Display All Tabs \n" +
+          "5. Open Nested Tab \n" + "6. Clear All Tabs \n" + "7. Save Tabs \n" + "8. Import Tabs \n" + "9. Exit")
