56d6dd9c4f207c6445a66a1fd41d551986875ba1
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-10 05:56:03 +0200

    while loop in main function

--- midterm.py
+++ midterm.py
@@ -7,7 +7,7 @@ Created on Thu Nov  9 21:58:10 2023
 
 
 def displayMenu():
-    print("1. Opeb Tab \n" + "2. Close Tab \n" + "3. Switch Tab \n" +
+    print("1. Open Tab \n" + "2. Close Tab \n" + "3. Switch Tab \n" +
           "4. Display All Tabs \n" + "5. Open Nested Tab \n" +
           "6. Clear All Tabs \n" + "7. Save Tabs \n" + "8. Import Tabs \n" +
           "9. Exit")
@@ -19,6 +19,27 @@ def main():
     while choice != 9:
         displayMenu()
         choice = int(input("Please enter your choice: "))
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            pass
+        elif choice == 6:
+            pass
+        elif choice == 7:
+            pass
+        elif choice == 8:
+            pass
+        elif choice == 9:
+            print("You are exiting")
+        else:
+            print("Inavalid input !! \n" + "Try again")
+    print("You did exit the program")
 
 
 main()
