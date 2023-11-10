dd5d92321536cf0cf17864fc9bd5fe2988fe1a1a
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-10 22:53:08 +0200

    starting web scraping methode

--- midterm.py
+++ midterm.py
@@ -5,6 +5,9 @@ Created on Thu Nov  9 21:58:10 2023
 @author: 1
 """
 
+from urllib.request import urlopen
+# urllib.request library for opening URLs
+
 
 def displayMenu():
     # O(1) function will always display a constant number of options which is 9
@@ -63,27 +66,20 @@ def closeTab(tab):
 
 
 def switchTab(tab):
+    # O(n) n is the number of elements of dict cause in worst case senario we will iterate thru all values
     # same as closeTab function check for index if is higher or equal to number of element switch the last tab
     index = int(input("Enter the index of tab that you want to close: "))
-    key_list = list(tab.keys())
-    if index >= len(key_list):
-        pass
+    url_list = list(tab.values())
+    if index >= len(url_list):
+        url = url_list[-1]
+        html_code = urlopen(url).read().decode("utf-8")
+        print(html_code)
     else:
         # searching for the inex in key list
-        for i in range(len(key_list)):
+        for i in range(len(url_list)):
             # if the index is finded => remove it
             if index == i + 1:
                 pass
-    return tab
-
-
-def clearAllTabs(tab):
-    # O(n) n is the number of opened tabs
-    # adding the keys of dict to a list
-    key_list = list(tab.keys())
-    # iterating thru the length of list and removing each key with corresponding value
-    for i in range(len(key_list)):
-        del tab[key_list[i]]
     return tab
 
 
@@ -96,6 +92,16 @@ def displayTabs(tab):
         # [0] to access the first item of value which is the title
         list_of_titles.append(value[0])
     return list_of_titles
+
+
+def clearAllTabs(tab):
+    # O(n) n is the number of opened tabs
+    # adding the keys of dict to a list
+    key_list = list(tab.keys())
+    # iterating thru the length of list and removing each key with corresponding value
+    for i in range(len(key_list)):
+        del tab[key_list[i]]
+    return tab
 
 
 def main():
