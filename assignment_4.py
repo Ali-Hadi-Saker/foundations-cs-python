392bb545e84b66e78e368d87aca997c0201a8e0d
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-19 10:35:35 +0200

    display menu of singlyLL

--- assignment_4.py
+++ assignment_4.py
@@ -5,10 +5,24 @@ Created on Sun Nov 19 09:28:59 2023
 @author: 1
 """
 
+# display this menu if user choose choice 1
+
+
+def displayMenu2():
+    print("a. Add Node\n" + "b. Display Nodes\n" +
+          "c. Search for & Delete Node\n" + "d. Return to main\n")
+
+
+def singlyLL():
+    choice_2 = 0
+    while choice_2 != 'd':
+        displayMenu2()
+        choice_2 = int(input("Peek your choice: "))
+
 
 def displayMenu():
     print("\n1. Singly Linked List\n" + "2. Check if Palindrome\n" +
-          "3. Priority Queu\n" + "4. Evalute an Inflix Expression\n" + "5. Graph\n" + "6. Exit")
+          "3. Priority Queu\n" + "4. Evalute an Inflix Expression\n" + "5. Graph\n" + "6. Exit\n")
 
 
 def main():
@@ -18,10 +32,8 @@ def main():
     while choice != 6:
         displayMenu()
         choice = int(input("Please enter your choice from the list above: "))
-        if choice == 0:
-            pass
-        elif choice == 1:
-            pass
+        if choice == 1:
+            singlyLL()
         elif choice == 2:
             pass
         elif choice == 3:
