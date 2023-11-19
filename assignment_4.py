6ec13f6fa0abe2607e3f96c4af0bb29f9fe90931
Ali-Hadi-Saker <alihady530@gmail.com>
2023-11-19 09:32:22 +0200

    starting assignment_4

---
+++ assignment_4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 19 09:28:59 2023
+
+@author: 1
+"""
+
+
+def displayMenue():
+    choice = 0
+    while choice != 6:
+        pass
+
+
+def main():
+    pass
+
+
+main()
--- test.py
+++ test.py
@@ -5,4 +5,36 @@ Created on Sat Nov  4 23:39:44 2023
 @author: 1
 """
 
-print("lessd2ey")
+
+class Car:
+    def __init__(self, color, model, year):
+        self.color = color
+        self.model = model
+        self.year = year
+
+    def Honk(self):
+        print("my car of type", self.model, "and color",
+              self.color, "and year", self.year, "goes beep beep")
+
+    def getColor(self):
+        return self.color
+
+    def setModel(self, model):
+        self.model = model
+
+    def getModel(self):
+        return self.model
+
+
+def main():
+    car1 = Car("read", "ferrari", "2010")
+    car2 = Car("blue", "marcedes", "2020")
+    car1.Honk()
+    car2.getColor()
+    print("car2 model is", car2.getModel())
+    print("car2 color is", car2.getColor())
+    car2.setModel("toyota")
+    print("car2 model is", car2.getModel())
+
+
+main()
